fc51859282244361ede0ef0029cdfacea9988b44
Ben <benperry1915@gmail.com>
2023-11-05 15:59:23 +0000

    Add date-fns library and implement date formatting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { createTodo } from "./todo.factory.js";
 import { createProject } from "./project.factory.js";
+import { format, toDate } from "date-fns"
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('DEVELOPMENT MODE');
@@ -8,16 +9,26 @@ if (process.env.NODE_ENV !== 'production') {
 
 /*
     this is to imitate real storage and show an example of the todo app working
+    
+    different date formats:
+      cccc do MMMM yyyy => Tuesday 2nd November 2023
+      ccc do MMM yy => Tue 5th Nov 23
+      dd/MM/yy =>  02/11/23
 */
+const dateFormat = "ccc do MMM yy";
+
 const proj1 = createProject("Example Project");
-proj1.addTask(createTodo("Title 1", "First Project", "01/01", 3)); 
-proj1.addTask(createTodo("Title 2", "Second Project", "02/01", 1));
-proj1.addTask(createTodo("Title 3", "Third Project", "03/01", 2));
-proj1.addTask(createTodo("Car maintenance", "Change oil and oil filter, air filter and cabin filter", "01/03", 3));
-proj1.addTask(createTodo("Replace hard drive", "A hard drive in NAS has went, replace before second harddrive fails and we lose our data!", "02/03", 3));
+proj1.addTask(createTodo("Title 1", "First Project", format(Date.now(), dateFormat), 3)); 
+proj1.addTask(createTodo("Title 2", "Second Project", format(Date.now(), dateFormat), 1));
+proj1.addTask(createTodo("Title 3", "Third Project", format(Date.now(), dateFormat), 2));
+proj1.addTask(createTodo("Car maintenance", "Change oil and oil filter, air filter and cabin filter", format(Date.now(), dateFormat), 3));
+proj1.addTask(createTodo("Replace hard drive", "A hard drive in NAS has went, replace before second harddrive fails and we lose our data!", format(Date.now(), dateFormat), 3));
 proj1.addTask(createTodo("Wash car", "Take it to the car wash on 34th Street, it's pretty dirty!", "", 1));
-proj1.addTask(createTodo("Dog walk!", "Walk dog", "03/03", 1));
+proj1.addTask(createTodo("Dog walk!", "Walk dog", format(Date.now(), dateFormat), 1));
 
 let task = proj1.getTask("Title 1");
 task.changeStatus();
 task.logValues();
+proj1.displayProject();
+proj1.removeTask("Title 2");
+proj1.displayProject();
--- src/project.factory.js
+++ src/project.factory.js
@@ -1,22 +1,20 @@
-import { createTodo } from "./todo.factory.js"
-
 function createProject(title="Title") {
     let projects = [];
 
     const getTitle = () => title;
     const changeTitle = (newTitle) => title = newTitle;
     //must be able to get todos from their unique title
-    const getTask = (taskTitle) => {
-        return projects.filter((task) => task.getTitle() === taskTitle)[0];
-    }
+    const getTask = (taskTitle) => 
+        projects.filter((task) => task.getTitle() === taskTitle)[0];
+    
     const addTask = (todo) => projects.push(todo);
-    const removeTask = (taskTitle) => {
+    const removeTask = (taskTitle) => 
         projects = projects.filter((task) => task.getTitle() !== taskTitle);
-    }
+    
 
     //temp function to display the project for now before going to dom manipulation
     const displayProject = () => {
-        console.log(title);
+        console.log("\n"+title);
         projects.forEach((proj) => proj.logValues());
     }
 
